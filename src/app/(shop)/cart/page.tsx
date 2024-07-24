7419545832b82976d5fb3710717922d1019de251
Ernesto Burgos <hdst.bur@gmail.com>
2024-07-24 18:05:16 -0300

    Pantalla del carrito de compras

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,7 +1,48 @@
+import { QuantitySelector, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import Link from "next/link";
+
+const productsInCart = [
+	initialData.products[0],
+	initialData.products[1],
+	initialData.products[2],
+];
+
 export default function CartPage() {
 	return (
-		<div>
-			<h1>Cart Page</h1>
+		<div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+			<div className="flex flex-col w-[1000px] ">
+				<Title title="Carrito" />
+				<div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+					{/* Carrito */}
+					<div className="flex flex-col mt-5">
+						<span className="text-xl">Agregar más ítems</span>
+						<Link href={"/"} className="underline mb-5">
+							Continúa comprando
+						</Link>
+					</div>
+					{/* Ítems */}
+					{productsInCart.map((product) => (
+						<div key={product.slug} className="flex">
+							<Image
+								src={`/products/${product.images[0]}`}
+								alt={product.title}
+								width={100}
+								height={100}
+								className="mr-5 rounded"
+							/>
+							<div>
+								<p>{product.title}</p>
+								<p>${product.price.toFixed(2)}</p>
+								<QuantitySelector quantity={3} />
+								<button className="underline mt-3">Remover</button>
+							</div>
+						</div>
+					))}
+					{/* Checkout */}
+				</div>
+			</div>
 		</div>
 	);
 }
