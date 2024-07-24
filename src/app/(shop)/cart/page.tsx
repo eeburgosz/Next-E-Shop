7ac181a3a4a9ba824344425bc528f8884cca1ebb
Ernesto Burgos <hdst.bur@gmail.com>
2024-07-24 18:18:14 -0300

    Carrito de compras parte 2

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -21,26 +21,55 @@ export default function CartPage() {
 						<Link href={"/"} className="underline mb-5">
 							Continúa comprando
 						</Link>
-					</div>
-					{/* Ítems */}
-					{productsInCart.map((product) => (
-						<div key={product.slug} className="flex">
-							<Image
-								src={`/products/${product.images[0]}`}
-								alt={product.title}
-								width={100}
-								height={100}
-								className="mr-5 rounded"
-							/>
-							<div>
-								<p>{product.title}</p>
-								<p>${product.price.toFixed(2)}</p>
-								<QuantitySelector quantity={3} />
-								<button className="underline mt-3">Remover</button>
+
+						{/* Ítems */}
+						{productsInCart.map((product) => (
+							<div key={product.slug} className="flex mb-5">
+								<Image
+									src={`/products/${product.images[0]}`}
+									alt={product.title}
+									width={100}
+									height={100}
+									style={{
+										height: "100px",
+										width: "100px",
+									}}
+									className="mr-5 rounded"
+								/>
+								<div>
+									<p>{product.title}</p>
+									<p>${product.price.toFixed(2)}</p>
+									<QuantitySelector quantity={3} />
+									<button className="underline mt-3">Remover</button>
+								</div>
 							</div>
+						))}
+					</div>
+					{/* Checkout - Resumen de orden */}
+					<div className="bg-white rounded-xl shadow-xl p-7">
+						<h2 className="text-2xl mb-2">Resumen de orden</h2>
+						<div className="grid grid-cols-2">
+							<span>No. de productos</span>
+							<span className="text-right">{3} artículos</span>
+
+							<span>Subtotal</span>
+							<span className="text-right">$100</span>
+
+							<span>Impuestos (15%)</span>
+							<span className="text-right">$15</span>
+
+							<span className="text-2xl mt-5">Total:</span>
+							<span className="text-right text-2xl mt-5">$115</span>
 						</div>
-					))}
-					{/* Checkout */}
+						<div className="mt-5 mb-2 w-full">
+							<Link
+								href={"/checkout/address"}
+								className="flex btn-primary justify-center"
+							>
+								Checkout
+							</Link>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
