5854a1106acb2bacbdfe93806b44fa2dffadac6e
Ernesto Burgos <hdst.bur@gmail.com>
2024-07-24 14:02:30 -0300

    Inicio pantalla de producto

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,18 +1,38 @@
+import { ProductsGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
+
+const seedProducts = initialData.products;
 
 interface Props {
 	params: {
-		id: string;
+		id: Category;
 	};
 }
 
 export default function CategorytPage({ params }: Props) {
 	const { id } = params;
 
-	if (id === "kids") notFound();
+	// if (id === "kids") notFound();
+
+	const products = seedProducts.filter((product) => product.gender === id);
+
+	const labels: Record<Category, string> = {
+		men: "para hombres",
+		women: "para mujeres",
+		kid: "para niños",
+		unisex: "para todos",
+	};
+
 	return (
-		<div>
-			<h1>Category Page {id}</h1>
-		</div>
+		<>
+			<Title
+				title={`Artículos ${labels[id]}`}
+				subtitle={`Todos los productos`}
+				className="mb-2"
+			/>
+			<ProductsGrid products={products} />
+		</>
 	);
 }
--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,38 @@
-export default function ProductPage() {
+import { titleFont } from "@/config/fonts";
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
+
+interface Props {
+	params: {
+		slug: string;
+	};
+}
+
+export default function ProductPage({ params }: Props) {
+	const { slug } = params;
+	const product = initialData.products.find((product) => product.slug === slug);
+	if (!product) notFound();
+
 	return (
-		<div>
-			<h1>Product Page</h1>
+		<div className="mt-5 mb-20 grid grid-cols-1 sm:grid-cols-3 gap-3">
+			{/* Slideshow */}
+			<div className="col-span-1 sm:col-span-2">
+				<h1>Hola Mundo</h1>
+			</div>
+			{/* Detalles */}
+			<div className="col-span-1 px-5">
+				<h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+					{product.title}
+				</h1>
+				<p className="text-lg mb-5">${product.price.toFixed(2)}</p>
+				{/* Selector de tallas */}
+				{/* Selector de cantidad */}
+				{/* Botón */}
+				<button className="btn-primary my-5">Agregar al carrito</button>
+				{/* Descripción */}
+				<h3 className="font-bold text-sm">Descripción</h3>
+				<p className="font-light">{product.description}</p>
+			</div>
 		</div>
 	);
 }
