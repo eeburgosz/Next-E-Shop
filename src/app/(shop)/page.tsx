61607ef72cb5358bcc4b1f1d45c0092fa724d8c5
Ernesto Burgos <hdst.bur@gmail.com>
2024-07-27 21:13:12 -0300

    Paginación manual (ingresando la página manualmente por URL)

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,8 +1,17 @@
 import { getPaginatedProductsWithImages } from "@/actions";
 import { ProductsGrid, Title } from "@/components";
 
-export default async function Home() {
-	const { products } = await getPaginatedProductsWithImages();
+interface Props {
+	searchParams: {
+		page?: string;
+	};
+}
+
+export default async function Home({ searchParams }: Props) {
+	const page = searchParams.page ? parseInt(searchParams.page) : 1;
+	const { products } = await getPaginatedProductsWithImages({ page });
+
+	console.log({ searchParams });
 
 	return (
 		<>
