d66f316a4669c7107d0c37200fe120c8234a030f
Ernesto Burgos <hdst.bur@gmail.com>
2024-07-27 21:17:23 -0300

    Controlar cuando no hay productos en la página

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,5 +1,6 @@
 import { getPaginatedProductsWithImages } from "@/actions";
 import { ProductsGrid, Title } from "@/components";
+import { redirect } from "next/navigation";
 
 interface Props {
 	searchParams: {
@@ -10,6 +11,7 @@ interface Props {
 export default async function Home({ searchParams }: Props) {
 	const page = searchParams.page ? parseInt(searchParams.page) : 1;
 	const { products } = await getPaginatedProductsWithImages({ page });
+	if (products.length === 0) redirect("/");
 
 	console.log({ searchParams });
 
