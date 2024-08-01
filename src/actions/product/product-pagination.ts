8cdd4abebee497bbc8416e985d3d43b4ca96eec5
Ernesto Burgos <hdst.bur@gmail.com>
2024-08-01 15:20:09 -0300

    Paginación - Determinando el total de páginas

--- src/actions/product/product-pagination.ts
+++ src/actions/product/product-pagination.ts
@@ -15,6 +15,8 @@ export const getPaginatedProductsWithImages = async ({
 	if (page < 1) page = 1;
 
 	try {
+		//* 1. Obtener los productos
+
 		const products = await prisma.product.findMany({
 			take: take,
 			skip: (page - 1) * take,
@@ -28,9 +30,17 @@ export const getPaginatedProductsWithImages = async ({
 			},
 		});
 		// console.log(products);
+
+		//* 2. Obtener el total de páginas
+		// todo:
+		const totalCount = await prisma.product.count({});
+		const totalPages = Math.ceil(totalCount / take);
+
 		//! Lo regreso de la siguiente forma para que quede igual a mi interface Product.
 		//! Tambien lo regreso así porque más adelante le pondré otras propiedades como currentPage, totalPages...
 		return {
+			currentPage: page,
+			totalPages: totalPages,
 			products: products.map((product) => ({
 				...product,
 				images: product.ProductImage.map((image) => image.url),
