3981c43fbb1738906f7a851f944e23f6b10bf248
Ernesto Burgos <hdst.bur@gmail.com>
2024-08-12 19:51:15 -0300

    Formato de moneda

--- src/app/(shop)/cart/ui/OrderSummary.tsx
+++ src/app/(shop)/cart/ui/OrderSummary.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useCartStore } from "@/store";
+import { currencyFormat } from "@/utils";
 import { useEffect, useState } from "react";
 
 export const OrderSummary = () => {
@@ -24,13 +25,15 @@ export const OrderSummary = () => {
 					</span>
 
 					<span>Subtotal</span>
-					<span className="text-right">${subTotal}</span>
+					<span className="text-right">{currencyFormat(subTotal)}</span>
 
 					<span>Impuestos (15%)</span>
-					<span className="text-right">${tax}</span>
+					<span className="text-right">{currencyFormat(tax)}</span>
 
 					<span className="text-2xl mt-5">Total:</span>
-					<span className="text-right text-2xl mt-5">${total}</span>
+					<span className="text-right text-2xl mt-5">
+						{currencyFormat(total)}
+					</span>
 				</>
 			)}
 		</div>
