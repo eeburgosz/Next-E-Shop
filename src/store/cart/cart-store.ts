1b112df1edd748993471c39de3d45b17d0e1d664
Ernesto Burgos <hdst.bur@gmail.com>
2024-08-12 18:14:24 -0300

    Hacer persistente el carrito de compras

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -14,28 +14,63 @@ interface State {
 
 export const useCartStore = create<State>()(
 	//persist, //! Se encarga de grabar el store en el localstorage (genera un problema cuando hace la rehidratación)
-	(set, get) => ({
-		cart: [],
 
-		// Métodos
-		addProductToCart: (product: CartProduct) => {
-			const { cart } = get();
-			// 1. Revisar si el producto existe en el carrito con la talla seleccionada.
-			const productInCart = cart.some(
-				(item) => item.id === product.id && item.size === product.size
-			);
-			if (!productInCart) {
-				set({ cart: [...cart, product] });
-				return;
-			}
-			// 2. Sé que el producto existe por talla. Tengo qué incrementarlo.
-			const updatedCartProducts = cart.map((item) => {
-				if (item.id === product.id && item.size === product.size) {
-					return { ...item, quantity: item.quantity + product.quantity };
+	persist(
+		(set, get) => ({
+			cart: [],
+
+			// Métodos
+			addProductToCart: (product: CartProduct) => {
+				const { cart } = get();
+				// 1. Revisar si el producto existe en el carrito con la talla seleccionada.
+				const productInCart = cart.some(
+					(item) => item.id === product.id && item.size === product.size
+				);
+				if (!productInCart) {
+					set({ cart: [...cart, product] });
+					return;
 				}
-				return item;
-			});
-			set({ cart: updatedCartProducts });
-		},
-	})
+				// 2. Sé que el producto existe por talla. Tengo qué incrementarlo.
+				const updatedCartProducts = cart.map((item) => {
+					if (item.id === product.id && item.size === product.size) {
+						return { ...item, quantity: item.quantity + product.quantity };
+					}
+					return item;
+				});
+				set({ cart: updatedCartProducts });
+			},
+		}),
+		{
+			name: "shopping-cart",
+		}
+	)
 );
+
+// export const useCartStore = create<State>()(
+// 	//persist, //! Se encarga de grabar el store en el localstorage (genera un problema cuando hace la rehidratación)
+
+// 	(set, get) => ({
+// 		cart: [],
+
+// 		// Métodos
+// 		addProductToCart: (product: CartProduct) => {
+// 			const { cart } = get();
+// 			// 1. Revisar si el producto existe en el carrito con la talla seleccionada.
+// 			const productInCart = cart.some(
+// 				(item) => item.id === product.id && item.size === product.size
+// 			);
+// 			if (!productInCart) {
+// 				set({ cart: [...cart, product] });
+// 				return;
+// 			}
+// 			// 2. Sé que el producto existe por talla. Tengo qué incrementarlo.
+// 			const updatedCartProducts = cart.map((item) => {
+// 				if (item.id === product.id && item.size === product.size) {
+// 					return { ...item, quantity: item.quantity + product.quantity };
+// 				}
+// 				return item;
+// 			});
+// 			set({ cart: updatedCartProducts });
+// 		},
+// 	})
+// );
